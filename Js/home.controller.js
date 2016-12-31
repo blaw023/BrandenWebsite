a52c5106d7a98d1c3be8e77293dbd3e44f006af9
blaw023 <blawrence023@gmail.com>
2016-12-30 21:58:48 -0500

    Initial set up of front end files.
    There is already some basic function implemented in html and javascript
    files.

---
+++ Js/home.controller.js
@@ -0,0 +1,41 @@
+﻿(function (angular) {
+
+    angular.module('branden-site.module')
+            .controller('branden-home.controller',['SocialMedia', '$location', '$window', BrandenHomeController])
+
+    function BrandenHomeController(SocialMedia, $location, $window) {     
+        var vm = this;
+
+        //functions
+        vm.gotoLinkedln = gotoLinkedln;
+        vm.gotoTwitter = gotoTwitter;
+        vm.gotoInsta = gotoInsta;
+        
+        activate();
+
+
+        function activate()
+        {
+        }
+
+        function gotoLinkedln()
+        {
+            $window.location = SocialMedia.Linkedln;
+            return $window.location;
+        }
+
+        function gotoTwitter()
+        {
+            $window.location = SocialMedia.Twitter;
+            return $window.location;
+        }
+
+        function gotoInsta()
+        {
+            $window.location = SocialMedia.Instagram;
+            return $window.location;
+        }
+        
+    }
+
+}(angular));
