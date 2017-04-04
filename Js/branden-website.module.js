530d873c1ac3e2571f60319bb3a6a70d1640a3ae
blaw023 <blawrence023@gmail.com>
2017-04-03 20:58:50 -0400

    delete routes

--- Js/branden-website.module.js
+++
@@ -1,8 +0,0 @@
-﻿angular
-    .module('branden-site.module', [])
-        .constant('SocialMedia', {
-            Linkedln: 'https://www.linkedin.com/in/branden-lawrence-b0191b81',
-            Twitter: 'https://twitter.com/blaw023',
-            Instagram: 'https://www.instagram.com/blaw023'
-        })
-    
