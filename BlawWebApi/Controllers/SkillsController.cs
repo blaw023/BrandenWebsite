5af4e4a8a68cc0581cdde71b77b9f96ca3a3cca8
blaw023 <blawrence023@gmail.com>
2017-06-07 21:20:38 -0400

    Added Api calls/Repository for Skills
    Added Api calls/Repository for Skills

---
+++ BlawWebApi/Controllers/SkillsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using Blaw_Website.BlawEntityFramework.DBContext;
+using Blaw_Website.BlawEntityFramework.Models;
+using Blaw_Website.BlawWebApi.Repositories;
+using System.Linq;
+
+namespace Blaw_Website.BlawWebApi.Controllers
+{
+    [RoutePrefix("api/skills")]
+    public class SkillsController : ApiController
+    {
+        private BLContext context;
+
+        public SkillsController()
+        {
+            this.context = new BLContext();
+        }
+
+        [HttpGet, Route("getSkillsVideos")]
+        public HttpResponseMessage GetSkillsVideos()
+        {
+            SkillsRepository sr = new SkillsRepository(context);
+            IList<Skills> skillsVideoList = sr.returnSkillsVideos();
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, skillsVideoList);
+        }
+    }
+}
