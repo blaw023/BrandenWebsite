f882566631da71162b35ee31166e5a3c98707c17
blaw023 <blawrence023@gmail.com>
2017-06-17 23:05:57 -0400

    Added BooksSearchMapping.cs file to Entity.

---
+++ BlawEntityFramework/EntityMappings/BooksSearchMapping.cs
@@ -0,0 +1,19 @@
+﻿using Blaw_Website.BlawEntityFramework.Models;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Blaw_Website.BlawEntityFramework.EntityMappings
+{
+    public class BooksSearchMapping : EntityTypeConfiguration<BooksSearch>
+    {
+        public BooksSearchMapping()
+        {
+            this.HasKey(e => e.bookCode);
+            this.ToTable("books_search");
+
+            this.Property(e => e.bookName).HasColumnName("Book_Name").HasMaxLength(50);
+            this.Property(e => e.bookCode).HasColumnName("Book_Code");
+            this.Property(e => e.bookUrl).HasColumnName("Book_Amazon_Url");
+
+        }
+    }
+}
