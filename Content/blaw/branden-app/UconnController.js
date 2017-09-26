fd4913a162b1e2a379deef6e9e683227700a073b
blaw023 <blawrence023@gmail.com>
2017-09-25 20:40:02 -0400

    front end additions for sports dashboard
    added necessary front end files for sports dashboard

---
+++ Content/blaw/branden-app/UconnController.js
@@ -0,0 +1,25 @@
+﻿(function(angular){
+
+    angular.module("branden-site.module")
+             .controller('UconnController', ['$mdDialog', 'selectedPlayerStats', UconnController])
+
+    function UconnController($mdDialog, selectedPlayerStats) {
+        var vm = this;
+
+        vm.playerStats = selectedPlayerStats;
+
+        vm.hide = function () {
+            $mdDialog.hide();
+        };
+
+        vm.cancel = function () {
+            $mdDialog.cancel();
+        };
+
+        vm.answer = function (answer) {
+            $mdDialog.hide(answer);
+        };
+
+    }
+
+}(angular))
--- Content/blaw/branden-app/branden-website.routes.js
+++ Content/blaw/branden-app/branden-website.routes.js
@@ -1,68 +1,91 @@
-﻿	angular.module('branden-site.module')
-		.config(routeConfig);
-		//.run(function($rootScope) {
-		//	 $rootScope.$on("$stateChangeError", console.log.bind(console));
-		//});
+﻿angular.module('branden-site.module').config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
 
-	function routeConfig($urlRouterProvider, $stateProvider){
-		
-		$urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/');
 
-		$stateProvider.state('home', {
-			url: '/',
-			templateUrl: 'Content/blaw/branden-app/templates/home.html',
-			controller: 'branden-home.controller as vm',
-			resolve: {
-				Images: [
-					'branden-site.dataservice', function(service) {
-						return service.getImages();
-					}
-				]
-			}
-		});
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: 'Content/blaw/branden-app/templates/home.html',
+        controller: 'branden-home.controller as vm',
+        resolve: {
+            Images: [
+                'branden-site.dataservice', function (service) {
+                    return service.getImages();
+                }
+            ]
+        }
+    });
 
-		$stateProvider.state('branden-skills', {
-			url: '/skills',
-			templateUrl: 'Content/blaw/branden-app/templates/skills.html',
-			controller: 'branden-skills.controller as vm',
-            resolve: {
-                SkillsVideos: [
-                    'branden-site.dataservice', function (service) {
-                        return service.getSkillsVideos();
-                    }
-                ]
-}
-		});
+    $stateProvider.state('branden-skills', {
+        url: '/skills',
+        templateUrl: 'Content/blaw/branden-app/templates/skills.html',
+        controller: 'branden-skills.controller as vm',
+        resolve: {
+            SkillsVideos: [
+                'branden-site.dataservice', function (service) {
+                    return service.getSkillsVideos();
+                }
+            ]
+        }
+    });
 
-		$stateProvider.state('branden-investments', {
-			url: '/investments',
-			templateUrl: 'Content/blaw/branden-app/templates/investments.html',
-			controller: 'branden-investments.controller as vm',
-			resolve: {
-				InvestmentList: [
-					'branden-site.dataservice', function (service) {
-						return service.getInvestments();
-					}
-				]
-			}
-		});
+    $stateProvider.state('branden-investments', {
+        url: '/investments',
+        templateUrl: 'Content/blaw/branden-app/templates/investments.html',
+        controller: 'branden-investments.controller as vm',
+        resolve: {
+            InvestmentList: [
+                'branden-site.dataservice', function (service) {
+                    return service.getInvestments();
+                }
+            ]
+        }
+    });
 
-		$stateProvider.state('branden-books', {
-			url: '/books',
-			templateUrl: 'Content/blaw/branden-app/templates/branden-books.html',
-			controller: 'branden-books.controller as vm',
-			resolve: {			
-				Books: [
-					'branden-site.dataservice', function (service) {
-						return service.getBooks();
-					}
-				],
-				BooksSearch: [
-                    'branden-site.dataservice', function (service) {
-                        return service.getBooksSearchUrl();
-                    }
-				]
-			}
-		});
-			
-}
+    $stateProvider.state('branden-books', {
+        url: '/books',
+        templateUrl: 'Content/blaw/branden-app/templates/branden-books.html',
+        controller: 'branden-books.controller as vm',
+        resolve: {
+            Books: [
+                'branden-site.dataservice', function (service) {
+                    return service.getBooks();
+                }
+            ],
+            BooksSearch: [
+                'branden-site.dataservice', function (service) {
+                    return service.getBooksSearchUrl();
+                }
+            ]
+        }
+    });
+
+    $stateProvider.state('branden-dashboard', {
+        url: '/sports-dashboard',
+        templateUrl: 'Content/blaw/branden-app/templates/branden-dashboard.html',
+        controller: 'branden-dashboard.controller as vm',
+        resolve: {
+            ChelseaPlayers: [
+                'branden-site.dataservice', function (service) {
+                    return service.getPlayers();
+                }
+            ],
+
+            ChelseaFacts: [
+                'branden-site.dataservice', function (service) {
+                    return service.getChelseaFacts();
+                }
+            ],
+
+            UconnPlayerStats: [
+                'branden-site.dataservice', function (service) {
+                    return service.getUconnPlayerStats();
+                }
+            ]
+
+        }
+    });
+
+}]);
+//.run(function($rootScope) {
+//	 $rootScope.$on("$stateChangeError", console.log.bind(console));
+//});
